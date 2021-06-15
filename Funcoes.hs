030bf7c7e7e26f5ae766ad957a28e348b9d0cf1c
Alisson C. Schmidt <84454233+Atomiqui@users.noreply.github.com>
2021-06-15 04:28:56 +0000

    15/06 : 01:28

--- Funcoes.hs
+++ Funcoes.hs
@@ -2,13 +2,33 @@ module Funcoes where
 
 import Text.Printf
 
+leFloat :: IO(Float)
+leFloat = do 
+  f <- readLn
+  return f
+
 leInt :: IO(Int)
-leInt = do readLn
+leInt = do 
+  i <- readLn
+  return i
 
 leCord :: IO(Int, Int)
-leCord = do 
-  putStr "Use esse modelo (larg, alt), para informar a primeira coordenada da imagem: "
+leCord = do
   readLn
+
+--Verifica se o ponto passado está dentro do tamanho da imagem e corrige
+verifica :: Float -> Float -> (Int, Int) -> (Int, Int)
+verifica 0 0 (px, py) = (0, 0)
+verifica x y (px, py)
+  | px > floor x && py > floor y = (floor x, floor y)
+  | px > floor x && py <= floor y = (floor x, py)
+  | px <= floor x && py > floor y = (px, floor y)
+  | otherwise = (px, py)
+
+--Gera uma cor aleatória para o circulo
+geraCor :: Int -> Int -> String
+geraCor x y = (\x -> "rgb" ++ show x) (x, y, x+y, 1)
+
 
 -- Reaproveitadas dos arquivos SVG
 
--- Main.hs
+++ Main.hs
@@ -1,16 +1,29 @@
 import Text.Printf
 import Funcoes
 
+lstCirc :: Int -> Int -> Int -> Int -> [String]
+lstCirc n x y r = [svgCircle (x+w) (y+w) r (geraCor r (w*2)) | w <- [(x-y+r), (x-y+r)+3..(x-y+r)+(3*n)]]
+
 main :: IO ()
 main = do
   --putStrLn "Informe a largura maxima da imagem: "
-  
+  --x <- leFloat
 
   --putStrLn "Informe a altura maxima da imagem: "
-  
+  --y <- leFloat
 
-  putStrLn "Informe um raio: "
+  putStrLn "Informe a coordenada central do circulo, neste formato: (x, y)."
+  cordxy <- leCord
+
+  putStrLn "Informe um raio qualquer: "
   r <- leInt
 
-  writeFile "img.svg" (arquivo)
-  where arquivo = svgBegin (leInt) (leInt) ++ (svgCircle 120 50 50 "rgb(105, 14, 30, 0.3)") ++ svgEnd
+  putStrLn "Informe quantos circulos deseja usar: "
+  n <- leInt
+  
+  let (px, py) = verifica 500 500 cordxy
+  let arquivo = concat ([svgBegin 500 500] ++ (lstCirc n px py r) ++ [svgEnd])
+   in writeFile "img.svg" arquivo
+
+--[(svgCircle (n^2) (n*2) r (geraCor n r)), (svgCircle (n^2) (n*2) r (geraCor n r))]
+--
